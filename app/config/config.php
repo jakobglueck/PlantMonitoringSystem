3e67238fecd10d6d2479f2bcebfd027ccce9ea23
jakobglueck <jakob.glueck4@gmail.com>
2025-03-10 20:42:48 +0100

    Füge Projektstruktur (app, public, etc.) hinzu

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+// Konfigurationsdatei
+$config = [
+    'db_host' => 'localhost',
+    'db_name' => 'PMS',
+    'db_user' => 'Soenke',
+    'db_pass' => '12341234'
+];
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+// Front-Controller: Alle Anfragen laufen hierüber
+require_once __DIR__ . '/../app/config/config.php';
+
+// Hier kann später ein Router eingebunden werden
+echo "Willkommen im PlantMonitoringSystem!";
